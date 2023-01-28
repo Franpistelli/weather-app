bfd81decf9952e32ba36cf9cafebdc166728c6b4
Franpistelli <franpistelli36@gmail.com>
2023-01-28 00:46:52 -0300

    Added new font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
       colors: {
-        'principal': '#1E213A',
+        principal: '#1E213A',
         'principal-dark': '#100E1D',
         'principal-text': '#E7E7EB',
         'principal-medium-text': '#A09FB1',
         'principal-dark-text': '##88869D'
-      },
+      }
     },
+    fontFamily: {
+      raleway: ['raleway', 'sans-serif']
+    }
   },
-  plugins: [],
+  plugins: []
 }
